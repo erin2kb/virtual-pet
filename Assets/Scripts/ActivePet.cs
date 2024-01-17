d093a71311f96612f44b835949cc2af8fac06eba
Erin Blackmere <erinkb@gmail.com>
2024-01-17 00:02:06 -0500

    Hook up evolution requirements UI

--- Assets/Scripts/ActivePet.cs
+++ Assets/Scripts/ActivePet.cs
@@ -8,6 +8,8 @@ public class ActivePet : MonoBehaviour
 {
     private GrowthValues currentValues = new GrowthValues();
 
+    private Pet currentPet;
+
     [SerializeField]
     public PetLine petLine;
 
@@ -35,8 +37,11 @@ public class ActivePet : MonoBehaviour
     [SerializeField]
     public TextMeshProUGUI exerciseText;
 
-    void Update() {
-        Pet currentPet = petLine.stages[petStage];
+    [SerializeField]
+    private Color requirementsMetColor = new Color(0.18f, 0.64f, 0.11f);
+
+    void OnEnable() {
+        currentPet = petLine.stages[petStage];
         nameText.SetText(currentPet.petName);
 
         if (petStage == PetStage.EGG) {
@@ -47,6 +52,20 @@ public class ActivePet : MonoBehaviour
 
         petImage.sprite = currentPet.sprite;
 
-        careText.SetText($"{currentValues.GetGrowthValue(GrowthCategory.CARE)} / {currentPet.evolutionRequirements.GetGrowthValue(GrowthCategory.CARE)}");
+        UpdateRequirementsText(careText, GrowthCategory.CARE);
+        UpdateRequirementsText(foodText, GrowthCategory.FOOD);
+        UpdateRequirementsText(groomingText, GrowthCategory.GROOMING);
+        UpdateRequirementsText(exerciseText, GrowthCategory.EXERCISE);
+    }
+
+    private void UpdateRequirementsText(TextMeshProUGUI text, GrowthCategory category) {
+        int currentValue = currentValues.GetGrowthValue(category);
+        int requiredValue = currentPet.evolutionRequirements.GetGrowthValue(category);
+
+        text.SetText($"{currentValue} / {requiredValue}");
+
+        if (currentValue >= requiredValue) {
+            text.color = requirementsMetColor;
+        }
     }
 }
